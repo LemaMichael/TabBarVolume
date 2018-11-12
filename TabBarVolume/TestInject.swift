7705204e6ce5b96a940c1e67d028eb07dcaa41fe
Michael Lema <mel526@nyu.edu>
2018-11-11 20:31:43 -0500

    Setting up project

---
+++ TabBarVolume/TestInject.swift
@@ -0,0 +1,35 @@
+//
+//  TestInject.swift
+//  TabBarVolume
+//
+//  Created by Michael Lema on 11/11/18.
+//  Copyright © 2018 Michael Lema. All rights reserved.
+//
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    #if DEBUG
+    @objc func injected() {
+        
+        if self is UITabBarController == false {
+            for subview in self.view.subviews {
+                subview.removeFromSuperview()
+            }
+            for sublayer in self.view.layer.sublayers ?? [] {
+                sublayer.removeFromSuperlayer()
+            }
+        }
+        
+        self.awakeFromNib()
+        self.loadView()
+        self.viewDidLoad()
+        self.reloadInputViews()
+        
+        if self is UITableViewController, let tableViewController = self as? UITableViewController {
+            tableViewController.tableView.reloadData()
+        }
+    }
+    #endif
+}
---
+++ TabBarVolume/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TabBarVolume
+//
+//  Created by Michael Lema on 11/11/18.
+//  Copyright © 2018 Michael Lema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
